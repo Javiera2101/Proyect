11e48de7e08bcc882479ff1a7111dfc0e8890aab
criz8766 <cristobal.ricciardi2001@alumnos.ubiobio.cl>
2024-11-06 07:29:16 -0300

    home controller

--- Proyecto-main/proyecto/demo/src/main/java/com/example/demo/controller/HomeController.java
+++ Proyecto-main/proyecto/demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(HttpSession session, Model model) {
+        String tipoUsuario = (String) session.getAttribute("tipoUsuario");
+        String nombreUsuario = (String) session.getAttribute("nombreUsuario");
+        
+        if (tipoUsuario != null && nombreUsuario != null) {
+            model.addAttribute("nombreUsuario", nombreUsuario);
+            return "index-logged";
+        } else {
+            return "index";
+        }
     }
 }
