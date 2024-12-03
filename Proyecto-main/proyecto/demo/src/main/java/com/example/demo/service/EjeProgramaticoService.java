ae10601467aeacf07b29579467b402aa2e9cc621
criz876 <cristobal.ricciardi2001@alumnos.ubiobio.cl>
2024-12-03 13:12:49 -0300

    proyectos

--- Proyecto-main/proyecto/demo/src/main/java/com/example/demo/service/EjeProgramaticoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Academico;
-import com.example.demo.model.ejeProgramatico;
-
-public interface  EjeProgramaticoService {
-    ejeProgramatico CrearejeProgramatico(ejeProgramatico ejeprogramatico, Academico academico);
-    
-}
---
+++ proyecto-main/proyecto/demo/src/main/java/com/example/demo/model/Proyectos.java
@@ -0,0 +1,98 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "proyecto")
+public class EjeProgramatico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proyecto")
+    private int idProyecto;
+
+    @Column(name = "tipo")
+    private String tipoProyecto;
+
+    @Column(name = "estado_proyecto")
+    private String estadoProyecto;
+
+    @Column(name = "fecha_inicio")
+    private String FechaInicioProyecto;
+
+    @Column(name = "fecha_fin")
+    private String FechaFinProyecto;
+
+    @Column(name = "nombre")
+    private String nombreProyecto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_polo")
+    private Polo polo;
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public String getTipoProyecto() {
+        return tipoProyecto;
+    }
+
+    public void setTipoProyecto(String tipoProyecto) {
+        this.tipoProyecto = tipoProyecto;
+    }
+
+    public String getEstadoProyecto() {
+        return estadoProyecto;
+    }
+
+    public void setEstadoProyecto(String estadoProyecto) {
+        this.estadoProyecto = estadoProyecto;
+    }
+
+    public String getFechaInicioProyecto() {
+        return FechaInicioProyecto;
+    }
+
+    public void setFechaInicioProyecto(String fechaInicioProyecto) {
+        FechaInicioProyecto = fechaInicioProyecto;
+    }
+
+    public String getFechaFinProyecto() {
+        return FechaFinProyecto;
+    }
+
+    public void setFechaFinProyecto(String fechaFinProyecto) {
+        FechaFinProyecto = fechaFinProyecto;
+    }
+
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
+
+    public Polo getPolo() {
+        return polo;
+    }
+
+    public void setPolo(Polo polo) {
+        this.polo = polo;
+    }
+
+    
+    
+}
