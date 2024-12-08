3326a44f562a965e024ac2cde505f9bfe7c62043
criz876 <cristobal.ricciardi2001@alumnos.ubiobio.cl>
2024-12-08 18:40:09 -0300

    cambios crear proyectos

--- proyecto-main/proyecto/demo/src/main/java/com/example/demo/controller/ProyectosController.java
+++ proyecto-main/proyecto/demo/src/main/java/com/example/demo/controller/ProyectosController.java
@@ -2,12 +2,10 @@ package com.example.demo.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -76,20 +74,5 @@ public class ProyectosController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error al crear el proyecto: " + e.getMessage());
         }
-    }
-
-    @GetMapping("/listar")
-    public String listarProyectos(Model model, HttpSession session) {
-        String correoUsuario = (String) session.getAttribute("correoUsuario");
-
-        if (correoUsuario == null) {
-            return "redirect:/login";
-        }
-
-        Polo polo = poloService.buscarPorCorreo(correoUsuario);
-        List<Proyectos> proyectos = proyectosService.obtenerProyectosPorPolo(polo);
-        model.addAttribute("proyectos", proyectos);
-
-        return "listar-proyectos";
     }
 }
